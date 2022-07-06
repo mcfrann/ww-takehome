e94893d2968658c729d35a5ab83e0fa8bde063d1
michaelvickdev <michael.vick.hello@gmail.com>
2022-07-06 18:30:12 +0500

    Added Visit Section

--- components/VisitSection/visitSection.js
+++ components/VisitSection/visitSection.js
@@ -1,25 +1,70 @@
 import styles from "./visit.module.scss";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { useRef, useState, useEffect } from "react";
-import { gsap } from "gsap";
-const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
+import { ImageOrSvg } from "../ImageorSvg/imageOrSvg";
 
 export const VisitSection = ({ section }) => {
-  const sectionRef = useRef();
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    // when the sectionRef hits the top of the page the imageColumn needs to lock
-    // into place until you get through the info Column scroll length
-  }, [sectionRef]);
+  const {
+    sectionImage,
+    hoursHeadline,
+    hoursGroups,
+    parallaxIconOne,
+    parallaxIconTwo,
+    addressLine1,
+    addressLine2,
+    zip,
+    menuTitle,
+  } = section.fields;
 
   return (
-    <div className={styles.visitSection} ref={sectionRef}>
+    <div id={menuTitle} className={styles.visitSection}>
       <div className={styles.infoColumn}>
-        <div className={styles.hoursSection}></div>
-        <div className={styles.visitSection}></div>
+        <div className={styles.hoursSection}>
+          <h3 className={styles.hoursHeadline}>{hoursHeadline}</h3>
+
+          {hoursGroups.map((_field, index) => {
+            const field = _field.fields;
+            return (
+              <div key={index}>
+                <h2 className={styles.hoursTitle}>{field.title}</h2>
+                {field.hours.map((_hours, _index) => {
+                  const hours = _hours.fields;
+
+                  return (
+                    <div key={_index} className={styles.hoursContainer}>
+                      <p>
+                        {hours.startDay}-{hours.endDay}
+                      </p>
+                      <p>
+                        {hours.startTime}M-{hours.endTime}M
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+          <div className={styles.parallaxImage}>
+            <ImageOrSvg image={parallaxIconOne} />
+          </div>
+        </div>
+        <div className={styles.visitSection}>
+          <h2 className={styles.hoursTitle}>VISIT</h2>
+          <div className={styles.hoursContainer}>
+            <p>
+              {addressLine1} {addressLine2}
+            </p>
+            <p>{zip}</p>
+          </div>
+          <div className={styles.mapsContainer} />
+          <div className={styles.parallaxImage}>
+            <ImageOrSvg image={parallaxIconTwo} />
+          </div>
+        </div>
       </div>
-      <div className={styles.imageColumn}></div>
+      <div className={styles.imageColumn}>
+        <div className={styles.image}>
+          <ImageOrSvg image={sectionImage} />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ config.js
@@ -0,0 +1,1 @@
+{ "spaceId" :"eonhjq7xzu1g", "environmentId": "master", "contentOnly": true, "queryEntries": [ "content_type=#######", // Content Model ID "sys.id=########" // Content ID ] }
