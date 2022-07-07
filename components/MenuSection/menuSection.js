32038f227756f4597878c8af25a63de515c7e7a0
michaelvickdev <michael.vick.hello@gmail.com>
2022-07-07 22:47:11 +0500

    Added menu section

--- components/MenuSection/menuSection.js
+++ components/MenuSection/menuSection.js
@@ -3,21 +3,21 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { ImageOrSvg } from "../ImageOrSvg/imageOrSvg.js";
 
 export const MenuSection = ({ section }) => {
-  const {
-    headline,
-    menuDownloadLink,
-    foodHighlights,
-    menuImage,
-    parallaxIconOne,
-  } = section.fields;
-
+  const { menuImage, parallaxIconOne, menuTitle } = section.fields;
   return (
-    <div className={styles.menuSection}>
+    <div id={menuTitle} className={styles.menuSection}>
       {menuImage && (
         <div className={styles.menuEmbedImage}>
           <ImageOrSvg image={menuImage} />
         </div>
       )}
+      <div className={styles.iconsContainer}>
+        {parallaxIconOne && (
+          <div className={styles.parallaxIconOne}>
+            <ImageOrSvg image={parallaxIconOne} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
