5d3cd529d80c6aa6c92a91ce5e5d5bfcfd175bc4
michaelvickdev <michael.vick.hello@gmail.com>
2022-07-15 04:15:28 +0800

    add parallax icons

--- components/IntroSection/introSection.js
+++ components/IntroSection/introSection.js
@@ -4,45 +4,68 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import React from "react";
 
-export const IntroSection = ({ section }) => {
+export const IntroSection = ({ section, parallax }) => {
   const {
     logoIcon,
     headline,
     subhead,
     mainImage,
     mainImageIcon,
+<<<<<<< HEAD
     parallaxIconOne,
     parallaxIconTwo,
+=======
+    parallaxOne,
+    parallaxTwo
+>>>>>>> 7db4e2e... add parallax icons
   } = section.fields;
 
-  const sectionRef = React.useRef(null);
+  const iconOneRef = React.useRef(null);
+  const iconTwoRef = React.useRef(null);
 
-  // React.useEffect(() => {
-  //   gsap.registerPlugin(ScrollTrigger);
-  //   gsap.to(sectionRef, {
-  //     x:0,
-  //     scrollTrigger: {
-  //       trigger: sectionRef,
-  //       scrub: true,
-  //       pin: true,
-  //       pinSpacing: false
-  //     }
-  //   });
-  // }, []);
+  React.useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(iconOneRef.current, {
+      y: "200px",
+      x:200,
+      duration: 1,
+      scrollTrigger: {
+        trigger: iconOneRef.current,
+        scrub: true,
+      }
+    });
+    gsap.from(iconTwoRef.current, {
+      y: "200px",
+      x:-200,
+      duration: 1,
+      scrollTrigger: {
+        trigger: iconTwoRef.current,
+        scrub: true,
+      }
+    });
+  }, []);
 
+  console.log(parallax);
   return (
-    <section className={`${styles.introSection} no-padding`} ref={sectionRef}>
+    <section className={`${styles.introSection} no-padding`}>
       <div className={styles.introContent}>
         {logoIcon && (
-          <div className={styles.logoIcon}>
+          <dic className={styles.logoIcon}>
             <ImageOrSvg image={logoIcon} />
-          </div>
+          </dic>
         )}
         <div className={styles.description}>
           <h1 className={styles.headline}>{headline}</h1>
           <h2 className={styles.subhead}>{subhead}</h2>
         </div>
+        <div className={styles.parallaxIconOne} ref={iconOneRef}>
+          <ImageOrSvg image={parallaxOne} />
+        </div>
+        <div className={styles.parallaxIconTwo} ref={iconTwoRef}>
+          <ImageOrSvg image={parallaxTwo} />
+        </div>
       </div>
+
       {mainImageIcon?.fields?.title && (
         <div className={styles.mainImageIconOuter}>
           <div className={styles.mainImageIcon}>
