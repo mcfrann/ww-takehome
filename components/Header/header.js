b37e94d68c8a7a79a586a518b7064e44fbbee52b
Liz Henderson <lhenderson303@gmail.com>
2022-07-14 08:29:05 +0100

    logo link to top, menu icon heirarchy and visit section padding

--- components/Header/header.js
+++ components/Header/header.js
@@ -3,15 +3,7 @@ import styles from "./header.module.scss";
 import { ImageOrSvg } from "../ImageorSvg/imageOrSvg.js";
 import { AnnouncementBar } from "../AnnouncementBar/announcementBar.js";
 import { useState } from "react";
-import {
-  Link,
-  Button,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
+import { Link } from "react-scroll";
 
 export const Header = ({ headerNav, footerNav, announcementBar }) => {
   const { logo, orderButton, headerSectionLinks } = headerNav.fields;
@@ -33,7 +25,16 @@ export const Header = ({ headerNav, footerNav, announcementBar }) => {
     const alt = logo.fields.title || "no alt description";
     return (
       <div className={styles.logoContainer}>
-        <Link className={styles.logoLink} href="/" to="/" title={alt}>
+        <Link
+          className={styles.logoLink}
+          activeClass="active"
+          to={"introSection"}
+          spy={true}
+          smooth={true}
+          offset={-150}
+          duration={500}
+          onSetActive={() => handleSetActive}
+        >
           <ImageOrSvg image={logo} />
         </Link>
       </div>
