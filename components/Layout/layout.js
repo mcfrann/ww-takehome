524fcbcce6573f6a5383b15e0991ff872a2d5f41
Liz Henderson <lhenderson303@gmail.com>
2022-07-26 11:54:21 -0600

    add favicon

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -11,7 +11,7 @@ export const Layout = ({
   metaDescription,
   footer,
   header,
-  announcementBar
+  announcementBar,
 }) => {
   return (
     <div className={styles.container} id={id}>
@@ -20,6 +20,27 @@ export const Layout = ({
         <meta name="description" content={metaDescription} />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.typekit.net/wlo5gtv.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <Header
