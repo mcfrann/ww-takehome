88331d8e20f8f250b4497c1602350fc8a71311f1
Francesca <fmcconnell59@gmail.com>
2022-10-18 14:46:50 -0500

    Add link to headline

--- components/AnnouncementBar/announcementBar.js
+++ components/AnnouncementBar/announcementBar.js
@@ -1,5 +1,9 @@
 import styles from './announcement.module.scss'
 
 export const AnnouncementBar = ({ headline, url, hide, hidden }) => {
-  return <div className={`${styles.announcementBar}`}>{headline}</div>
+  return (
+    <div className={`${styles.announcementBar}`}>
+      <a href={url}>{headline}</a>
+    </div>
+  )
 }
