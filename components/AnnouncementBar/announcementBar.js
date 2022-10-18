0a0cd952d96e8b96f3cd77d1a69c7192bab8e0be
Francesca <fmcconnell59@gmail.com>
2022-10-18 14:31:14 -0500

    Get hiring message showing

--- components/AnnouncementBar/announcementBar.js
+++ components/AnnouncementBar/announcementBar.js
@@ -1,5 +1,5 @@
-import styles from "./announcement.module.scss";
+import styles from './announcement.module.scss'
 
 export const AnnouncementBar = ({ headline, url, hide, hidden }) => {
-  return <div className={styles.announcementBar}></div>;
-};
+  return <div className={`${styles.announcementBar}`}>{headline}</div>
+}
