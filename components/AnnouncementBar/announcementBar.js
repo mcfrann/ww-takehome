c3988a14e2323f631d6ecf329afc251bd1011830
Francesca <fmcconnell59@gmail.com>
2022-10-18 14:51:11 -0500

    Determine classname based on hidden prop

--- components/AnnouncementBar/announcementBar.js
+++ components/AnnouncementBar/announcementBar.js
@@ -2,8 +2,14 @@ import styles from './announcement.module.scss'
 
 export const AnnouncementBar = ({ headline, url, hide, hidden }) => {
   return (
-    <div className={`${styles.announcementBar}`}>
-      <a href={url}>{headline}</a>
+    <div
+      className={
+        hidden ? `${styles.announcementBar}` : `${styles.announcementBar}`
+      }
+    >
+      <a href={url} onClick={hide}>
+        {headline}
+      </a>
     </div>
   )
 }
