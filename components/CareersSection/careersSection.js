d786414a15a2e8d5997cb9dab0f01036ea15d3ec
Francesca <fmcconnell59@gmail.com>
2022-10-20 12:17:50 -0500

    Add h1 to careers for reference

--- components/CareersSection/careersSection.js
+++ components/CareersSection/careersSection.js
@@ -1,6 +1,6 @@
-import styles from "./careers.module.scss";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import Marquee from "react-fast-marquee";
+import styles from './careers.module.scss'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Marquee from 'react-fast-marquee'
 
 export const CareersSection = ({ section }) => {
   const {
@@ -8,9 +8,13 @@ export const CareersSection = ({ section }) => {
     copy: _copy,
     applyButton,
     menuTitle,
-    buttonCtaCopy,
-  } = section.fields;
+    buttonCtaCopy
+  } = section.fields
 
-  const copy = documentToReactComponents(_copy);
-  return <section className={styles.careersSection} id={menuTitle}></section>;
-};
+  const copy = documentToReactComponents(_copy)
+  return (
+    <section className={styles.careersSection} id={menuTitle}>
+      <h1>Careers section would go here. :)</h1>
+    </section>
+  )
+}
